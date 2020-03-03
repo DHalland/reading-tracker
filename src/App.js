187fc0503951e931ea0bc4fafb61ede34f8a7e27
DHalland <dylanhalland@gmail.com>
2020-03-02 23:14:25 -0800

    login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Login from "./components/Login";
 
-function App() {
-   return <div className="App">Learn React</div>;
+class App extends Component {
+   render() {
+      return (
+         <div>
+            <div className="App" className="login">
+               Reading Tracker
+            </div>
+            <Login></Login>
+         </div>
+      );
+   }
 }
 
 export default App;
