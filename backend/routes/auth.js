66c9310a69e2500aed9326a3eaf37e84d10d632d
Keith Gordon <keithagordon6@gmail.com>
2020-03-29 20:48:14 -0700

    added route to view users for development

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -41,4 +41,11 @@ userRoutes.route('/').post((req, res) => {
 });
 
 
+//REMOVE AFTER TESTING
+userRoutes.route('/').get( (req, res) => {
+    User.find()
+        .then(users => res.json(users))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
 module.exports = userRoutes;
